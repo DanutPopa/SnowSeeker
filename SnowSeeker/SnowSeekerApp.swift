ebd18d1d8b86632cf5e02d8053a8f29b73cc029a
DanutPopa <popa.ion.danut@gmail.com>
2025-01-06 17:47:07 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Danut Popa on 06.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
