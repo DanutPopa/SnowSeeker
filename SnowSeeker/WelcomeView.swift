564e8f90c99b4370318c26274ac583b772feb18b
DanutPopa <popa.ion.danut@gmail.com>
2025-01-08 18:46:46 +0200

    Creating a detail view for resorts.

--- SnowSeeker/WelcomeView.swift
+++ SnowSeeker/WelcomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         VStack {
-            Text("Welcome to SnowSeeker")
+            Text("Welcome to SnowSeeker!")
                 .font(.largeTitle)
             
             Text("Please select a resort from the left-hand menu; swipe from the edge to show it")
